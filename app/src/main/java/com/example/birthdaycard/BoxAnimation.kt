d90a4231885cc4a64fb374f7f95f2803845d922e
jeetkariya77777 <jeetkariya77777@gmail.com>
2023-01-10 14:24:35 +0530

    box animation complete 2

--- app/src/main/java/com/example/birthdaycard/BoxAnimation.kt
+++ app/src/main/java/com/example/birthdaycard/BoxAnimation.kt
@@ -7,18 +7,16 @@ import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
 
